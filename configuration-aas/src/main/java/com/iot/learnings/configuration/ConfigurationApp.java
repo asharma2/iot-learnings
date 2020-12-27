15706a5592ef4490ddfc08af0b02e53d9fbea6ed
Atul Sharma <atulkumarsharma1986@gmail.com>
2020-12-28 01:20:36 +0530

    Adding the event workflow

---
+++ configuration-aas/src/main/java/com/iot/learnings/configuration/ConfigurationApp.java
@@ -0,0 +1,14 @@
+package com.iot.learnings.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigurationApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigurationApp.class, args);
+	}
+}
---
+++ gateway-aas/src/main/java/com/iot/learnings/gateway/GatewayApp.java
@@ -0,0 +1,14 @@
+package com.iot.learnings.gateway;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayApp {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
