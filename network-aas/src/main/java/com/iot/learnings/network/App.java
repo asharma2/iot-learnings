13c47ef0fad03268d2b171552d4ea170914387a8
Atul Sharma <atulkumarsharma1986@gmail.com>
2020-12-27 20:48:50 +0530

    Adding the event workflow

---
+++ network-aas/src/main/java/com/iot/learnings/network/App.java
@@ -0,0 +1,13 @@
+package com.iot.learnings.network;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
